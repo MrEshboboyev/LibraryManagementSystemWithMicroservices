e4a752c8b42f665365fb3efb4ab5f57d8d552289
MrEshboboyev <mreshboboyev@gmail.com>
2024-10-26 10:40:31 +0500

    Generic and entity repos;

---
+++ LibraryMS.Services.Membership.Application/Common/Interfaces/IMemberRepository.cs
@@ -0,0 +1,8 @@
+﻿using LibraryMS.Services.Membership.Domain.Entities;
+
+namespace LibraryMS.Services.Membership.Application.Common.Interfaces;
+
+public interface IMemberRepository : IRepository<Member>
+{
+}
+
---
+++ LibraryMS.Services.Membership.Application/Common/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,10 @@
+﻿namespace LibraryMS.Services.Membership.Application.Common.Interfaces;
+
+public interface IUnitOfWork
+{
+    IMemberRepository Member { get; }
+    IMembershipTypeRepository MembershipType { get; }
+    ILoanHistoryRepository LoanHistory { get; }
+    
+    Task SaveAsync();
+}
