354d6003becdf8280c5bda5b692777e64a6adb2e
MrEshboboyev <mreshboboyev@gmail.com>
2024-10-26 13:59:43 +0500

    Create a BaseService

---
+++ LibraryMS.Services.Membership.Application/Services/BaseService.cs
@@ -0,0 +1,10 @@
+﻿using AutoMapper;
+using LibraryMS.Services.Membership.Application.Common.Interfaces;
+
+namespace LibraryMS.Services.Membership.Application.Services;
+
+public abstract class BaseService(IUnitOfWork unitOfWork, IMapper mapper)
+{
+    protected readonly IUnitOfWork UnitOfWork = unitOfWork ?? throw new ArgumentNullException(nameof(unitOfWork));
+    protected readonly IMapper Mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+}
