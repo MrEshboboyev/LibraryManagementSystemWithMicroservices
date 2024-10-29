9435e680b6e00a71ee20504d862ceef6da07869b
MrEshboboyev <mreshboboyev@gmail.com>
2024-10-29 11:18:11 +0500

    Membership : default utilities;

---
+++ LibraryMS.Services.Membership.Application/Common/Utility/SD.cs
@@ -0,0 +1,12 @@
+﻿namespace LibraryMS.Services.Membership.Application.Common.Utility;
+
+public static class SD
+{
+    // Membership types
+    public const string MembershipTypeBasic = "Basic";
+    public const string MembershipTypeTrial = "Trial";
+    public const string MembershipTypePremium = "Premium";
+
+    // default expiration days
+    public const int expirationDays = 30;
+}
