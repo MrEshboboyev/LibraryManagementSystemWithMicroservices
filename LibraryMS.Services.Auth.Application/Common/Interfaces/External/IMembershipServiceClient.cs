8b20567b081f9488df7acfa2eb337652c5e23cdc
MrEshboboyev <mreshboboyev@gmail.com>
2024-10-29 10:44:37 +0500

    Auth : create external client (membership)

---
+++ LibraryMS.Services.Auth.Application/Common/Interfaces/External/IMembershipServiceClient.cs
@@ -0,0 +1,6 @@
+﻿namespace LibraryMS.Services.Auth.Application.Common.Interfaces.External;
+
+public interface IMembershipServiceClient
+{
+    Task<bool> CreateDefaultMembershipAsync(Guid userId);
+}
---
+++ LibraryMS.Services.Auth.Infrastructure/ExternalClients/MembershipServiceClient.cs
@@ -0,0 +1,16 @@
+﻿using LibraryMS.Services.Auth.Application.Common.Interfaces.External;
+using System.Net.Http.Json;
+
+namespace LibraryMS.Services.Auth.Infrastructure.ExternalClients;
+
+public class MembershipServiceClient(HttpClient httpClient) : IMembershipServiceClient
+{
+    private readonly HttpClient _httpClient = httpClient;
+
+    public async Task<bool> CreateDefaultMembershipAsync(Guid userId)
+    {
+        var response = await _httpClient.PostAsJsonAsync<bool>($"/api/memberships/create-default/{userId}", default);
+        return response.IsSuccessStatusCode;
+    }
+}
+
